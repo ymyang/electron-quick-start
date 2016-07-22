f938709deae0f78e67198f8279b717a0adddb0a3
ymyang <ymyang@163.com>
2016-07-22 20:59:14 +0800

    __dirname

--- src/app/main.js
+++ src/app/main.js
@@ -35,7 +35,7 @@ app.on('activate', function() {
 
 function createWindow() {
     win = new BrowserWindow({
-        icon: 'res/yliyun_64.png',
+        icon: __dirname + '/res/yliyun_64.png',
         title: '一粒云盘',
         width: 800,
         height: 600,
@@ -56,7 +56,7 @@ function createWindow() {
 }
 
 function initTray() {
-    tray = new Tray('res/yliyun.ico');
+    tray = new Tray(__dirname + '/res/yliyun.ico');
     tray.setToolTip('一粒云盘');
 
     tray.on('click', function() {
@@ -65,14 +65,14 @@ function initTray() {
 
     var menu = new Menu();
     menu.append(new MenuItem({
-        icon: 'res/yliyun_16.png',
+        icon: __dirname + '/res/yliyun_16.png',
         label: '打开云盘网页版',
         click: function() {
             electron.shell.openExternal('http://www.yliyun.com');
         }
     }));
     menu.append(new MenuItem({
-        icon: 'res/ios7-paperplane-outline-16.png',
+        icon: __dirname + '/res/ios7-paperplane-outline-16.png',
         label: '在线升级',
         click: function() {
             //
@@ -82,7 +82,7 @@ function initTray() {
         type: 'separator'
     }));
     menu.append(new MenuItem({
-        icon: 'res/off_black_16.png',
+        icon: __dirname + '/res/off_black_16.png',
         label: '退出',
         click: function() {
             //tray.destroy();
